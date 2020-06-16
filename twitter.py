4645a65f7c816831bde6e686719352135ee2ba07
iaryankashyap <aryank.kashyap77783@gmail.com>
2020-06-16 22:00:16 +0530

    Bot Ready

---
+++ twitter.py
@@ -0,0 +1,21 @@
+import tweepy
+import time
+
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+user = api.me()
+
+search = '100DaysOfCode'
+nrTweets = 500
+
+for tweet in tweepy.Cursor(api.search, search).items(nrTweets):
+    try:
+        print('Tweet Liked')
+        tweet.favorite()
+        tweet.retweet()
+        time.sleep(10)
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
