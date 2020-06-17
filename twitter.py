774ede57b7f61eb5b43e4f4d7a20fd11e8abec9f
iaryankashyap <aryank.kashyap77783@gmail.com>
2020-06-17 22:45:50 +0530

    Bot Ready

--- twitter.py
+++ twitter.py
@@ -1,6 +1,10 @@
 import tweepy
 import time
 
+auth = tweepy.OAuthHandler("nBzN5fIGo7eIac2dXzxEkmJTf",
+                           "QyRJWpA7sUwpXa3sMteB5RGXzHi6zEAdIHVUQAVeSOTTmnzEGH")
+auth.set_access_token("1272918562403782661-HgpnGSh9kVARsqozKdFI3sjZ4Ozd8o",
+                      "6zJNJanh4Y0SS8ffKjIJm28SShwu6J0ZqAUU5zYyGHDdq")
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
