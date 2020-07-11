58408c4f1e3b393cc31568b483d0ca28b0efc105
Aryan Kashyap <aryank.kashyap77783@gmail.com>
2020-07-11 18:29:46 +0530

    Update application.py

--- webapp/application.py
+++ webapp/application.py
@@ -14,10 +14,7 @@ app = Flask(__name__)
 def retlike():
     global n
     global m
-    auth = tweepy.OAuthHandler("nBzN5fIGo7eIac2dXzxEkmJTf",
-                               "QyRJWpA7sUwpXa3sMteB5RGXzHi6zEAdIHVUQAVeSOTTmnzEGH")
-    auth.set_access_token("1272918562403782661-HgpnGSh9kVARsqozKdFI3sjZ4Ozd8o",
-                          "6zJNJanh4Y0SS8ffKjIJm28SShwu6J0ZqAUU5zYyGHDdq")
+
 
     api = tweepy.API(auth, wait_on_rate_limit=True,
                      wait_on_rate_limit_notify=True)
